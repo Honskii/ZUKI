e166d8839b1ff6b1f7bffae8a4bf302fcddbe86b
Honskii <rarmir1200363rus@gmail.com>
2026-01-29 18:41:28 +0500

    Timedelta fix

--- plugins/skip_updates/updates/skip_messages.py
+++ plugins/skip_updates/updates/skip_messages.py
@@ -7,6 +7,6 @@ class MessagesUpdateSkipper:
 
     def should_skip(self, update: Update) -> bool:
         if isinstance(update, Message):
-            if (datetime.now(tz=update.date.tzinfo) - update.date).seconds > self.skip_interval:
+            if (datetime.now(tz=update.date.tzinfo) - update.date).total_seconds() > self.skip_interval:
                 return True
         return False
